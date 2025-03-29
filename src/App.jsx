3260007bcc9b94ae4f65537e7eb9e3a5f4efe5df
shabeeltt <shabeeltt.official@gmail.com>
2025-03-29 10:12:16 +0530

    created home dev2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Home from "./Component/Home/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>this is the new projectasdf ....</h1>
       <h2>added some paragraph from fasdjf</h2>
       <h1>added by Nikhil</h1>
+      <Home />
     </>
   );
 }

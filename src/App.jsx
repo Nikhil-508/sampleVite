ecfe50adfab24621065b868eb02cbb42d12de996
Nikhil <nikhilbchandran@gmail.com>
2025-03-29 10:11:19 +0530

    dev1

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./Pages/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1>this is the new projectasdf ....</h1>
-      <h2>added some paragraph from fasdjf</h2>
-      <h1>added by Nikhil</h1>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Navbar/>} />
+    </Routes>
+     
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      this is the navbar from dev 1
+    </div>
+  )
+}
+
+export default Navbar

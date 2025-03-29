bb4fa5528ae86f66605df47e021adabebd1f3b8d
shabeeltt <shabeeltt.official@gmail.com>
2025-03-29 10:03:24 +0530

    added a new heading dev2

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-
-    <h1>this is the new project ....</h1>
-
+      <h1>this is the new project ....</h1>
+      <h2>added some heading dev2</h2>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
